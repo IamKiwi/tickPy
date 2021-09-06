1ff105c01c0770ad3401b7102763e892fc071cc0
IamKiwi <sgolda9@gmail.com>
2021-09-06 15:35:33 +0200

    CR for categories of resources management

--- tick_app/urls.py
+++ tick_app/urls.py
@@ -4,6 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register_page, name='register'),
-    path('login/', views.login_page, name='login')
+    path('login/', views.login_page, name='login'),
+
+    path('new_ticket/', views.create_new_ticket, name='new_ticket'),
+    path('track_my_tickets/', views.customer_ticket_tracking, name='track_my_tickets'),
+
+    path('manage_resources/', views.admin_manage_resources, name='admin_manage_resources'),
+    # path('create_category/', views.create_category, name='create_category')
 
 ]
--- tick_app/views.py
+++ tick_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import *
+from .forms import *
 # Create your views here.
 
 
@@ -13,3 +15,29 @@ def register_page(request):
 
 def login_page(request):
     return render(request, 'tick_app/login.html')
+
+
+def create_new_ticket(request):
+    return render(request, 'tick_app/new_ticket_form.html')
+
+
+def customer_ticket_tracking(request):
+    return render(request, 'tick_app/customer_ticket_tracking.html')
+
+
+def admin_manage_resources(request):
+    categories = Category.objects.all()
+    categoryForm = CategoryForm()
+    if request.method == "POST":
+        print("Printing POST (amr): ", request.POST)
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            print('OKOKOKOKOKOKOK')
+            print(request.POST.get('category_name'))
+            form.save()
+            return redirect('admin_manage_resources')
+
+    context = {'categories': categories, 'form': categoryForm}
+    return render(request, 'tick_app/admin_manage_resources.html', context)
+
+
